f723450fb80576077a85b3ea1555d8e3f9539d05
= <=>
2026-04-24 11:20:15 -0500

    refactor: refactor imports

--- {{project_name}}/tests/endpoints/test_auth.py
+++ {{project_name}}/tests/endpoints/test_auth.py
@@ -1,12 +1,17 @@
+from typing import TYPE_CHECKING
 from fastapi.testclient import TestClient
-from models.user import User
-from crud.user import create_user, get_user_by_id
+from crud.user import create_user
 from core.security import hash_password
 from schemas.user import UserCreate
 
+if TYPE_CHECKING:
+    from models.user import User
+
 
 def test_login_user(db_test_session, client: TestClient, user_test: UserCreate) -> None:
-    _: User | None = create_user(db_test_session, user_test, hash_password(user_test.password))
+    _: User | None = create_user(
+        db_test_session, user_test, hash_password(user_test.password)
+    )
     form_data = {"username": user_test.username, "password": user_test.password}
     response = client.post(
         "/auth/login/",
